f95293744d04355ac460a46b03a5765174b7d47b
vfeofanov <vasya3000-95@mail.ru>
2024-11-14 18:22:46 +0100

    started to write new mano clas

--- src/mano/mano.py
+++ src/mano/mano.py
@@ -55,3 +55,64 @@ class MaNo(Base_alg):
                 break
         losses = torch.Tensor(loss)
         return torch.mean(losses)
+
+
+
+
+# NEW MANO
+class MaNo:
+    def __init__(self, threshold=5):
+        self.threshold = threshold
+
+    def evaluate(self):
+        self.base_model.train()
+        score_list = []
+
+        for batch_idx, batch_data in enumerate(self.val_loader):
+            inputs, labels = batch_data[0], batch_data[1]
+            inputs, labels = inputs.to(self.device), labels.to(self.device)
+
+            with torch.no_grad():
+                outputs = self.base_model(inputs)
+                outputs = self._normalize(outputs)
+                score = torch.norm(outputs, p=self.args["norm_type"]) / (
+                    (outputs.shape[0] * outputs.shape[1])
+                    ** (1 / self.args["norm_type"])
+                )
+            # final score
+            score_list.append(score)
+
+        scores = torch.Tensor(score_list).numpy()
+        return scores.mean()
+
+    def _normalize(self, logits):
+        # loss = self.args["delta"]
+        loss = self.uniform_cross_entropy()
+        if loss > self.threshold:
+            outputs = torch.softmax(logits, dim=1)
+        else:
+            outputs = logits + 1 + logits**2 / 2
+            min_value = torch.min(outputs, 1, keepdim=True)[0].expand_as(outputs)
+
+            # Remove min values to ensure all entries are positive. This is especially
+            # needed when the approximation order is higher than 2.
+            outputs = nn.functional.normalize(outputs - min_value, dim=1, p=1)
+        return outputs
+
+    def uniform_cross_entropy(self):
+        losses = []
+        for batch_idx, batch_data in enumerate(self.val_loader):
+            if batch_idx < 5:
+                inputs, labels = batch_data[0], batch_data[1]
+                inputs, labels = inputs.to(self.device), labels.to(self.device)
+                with torch.no_grad():
+                    logits = self.base_model(inputs)
+                    targets = torch.ones(
+                        (logits.shape[0], self.args["num_classes"])
+                    ).to(self.device) * (1 / self.args["num_classes"])
+                    loss = nn.functional.cross_entropy(logits, targets)
+                    losses.append(loss)
+            else:
+                break
+        losses = torch.Tensor(loss)
+        return torch.mean(losses)
